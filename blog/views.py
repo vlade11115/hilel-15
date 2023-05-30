fb594fc68aebc46918fbeab59e1eae53433a4376
Vladyslav Kartavets <vladyslav.kartavets@staircase.co>
2023-05-30 20:50:13 +0300

    Add students view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+from blog.models import Student
+
 
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+
+
+def students(request):
+    students = Student.objects.all()
+    return render(request, "students.html", {"students": students})
