0568520116b7d469ac92e6f24bc785e8c366b723
Vladyslav Kartavets <vladyslav.kartavets@staircase.co>
2023-06-09 19:43:17 +0300

    Add cert

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,19 @@
+import uuid
+
 from django.db import models
 
 
 # Create your models here.
 
 
+def upload_certificate(instance, filename):
+    return f"certificates/{uuid.uuid4()}/{filename}"
+
+
 class Student(models.Model):
     name = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
+    certificate = models.ImageField(upload_to=upload_certificate, null=True)
 
     def __str__(self):
         return self.name
